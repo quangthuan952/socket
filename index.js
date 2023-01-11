c1cfe2afb7b842c7cc7667508c5442933d8bf95b
thuanhq <thuanhq@wilad.vn>
2023-01-11 09:44:36 +0700

    update chat

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const io = require("socket.io")(8000, {
-    cors: {
-        origin: 'http://localhost:3000'
-    }
+  cors: {
+    origin: 'http://localhost:3000'
+  }
 })
 
-
+let onlineUsers = []
 io.on("connection", (socket) => {
-    socket.on("send-message", (data) => {
-
-    })
+  
+  socket.on("user-online", (userId) => {
+    if (!onlineUsers.some(user => user.userId === userId)) {
+      onlineUsers.push({userId, socketId: socket.id})
+    }
+    io.emit('get-users-online', onlineUsers)
+  })
+  
+  socket.on("disconnect", (userId) => {
+    onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
+    io.emit('get-users-online', onlineUsers)
+  })
+  
+  socket.on("send-message", (data) => {
+    const {receiverId} = data
+    const user = onlineUsers.find(user => user.userId === receiverId)
+    if (user) {
+      io.to(user.socketId).emit('recieve-message', data)
+    }
+  })
 })
